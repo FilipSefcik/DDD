5ae37b7a161e34b4bd4202d7cb2d1ab40ec72ce9
FilipSefcik <sefcik.filip02@gmail.com>
2024-11-20 08:37:36 +0100

    added divider and tested it

---
+++ DDD/utils/divider.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include "../modules/module.hpp"
+#include <algorithm>
+#include <cstdint>
+#include <iostream>
+#include <vector>
+
+/*
+ * @brief Divider assigns modules from parameter to processing nodes.
+ *
+ * Includes a `check` method for verifying input modules.
+ */
+class divider {
+  public:
+    virtual ~divider() = default;
+
+    virtual void divide_modules(std::vector<module*>* modules, std::vector<module*> nodes[],
+                                std::vector<int>* count) = 0;
+
+  protected:
+    void check(std::vector<module*>* modules); // Defined in the base class
+};
+
+/*
+ * @brief Divides modules among nodes evenly based on no particular property.
+ */
+class node_divider : public divider {
+  public:
+    void divide_modules(std::vector<module*>* modules, std::vector<module*> nodes[],
+                        std::vector<int>* count) override;
+};
+
+/*
+ * @brief Divides modules among nodes evenly based on how many variables modules have.
+ */
+class var_count_divider : public divider {
+  public:
+    void divide_modules(std::vector<module*>* modules, std::vector<module*> nodes[],
+                        std::vector<int>* count) override;
+};
