dc00d733c1074a93f00cd11a8a3d16b5d6155abd
FilipSefcik <sefcik.filip02@gmail.com>
2024-11-20 08:37:36 +0100

    added divider and tested it

--- DDD/main.cpp
+++ DDD/main.cpp
@@ -1,22 +1,37 @@
 #include "modules/module.hpp"
 #include "modules/module_manager.hpp"
+#include "utils/divider.hpp"
 #include <iostream>
+#include <new>
 #include <ostream>
+#include <vector>
 
 int main(int argc, char* argv[]) {
 
     module_manager manager;
     manager.load("../../DDD/load_files/modules/module_map.conf");
-    manager.print_modules();
-    int i = 0;
-    for (module* mod : *manager.get_modules()) {
-        mod->assign_process(i);
-        i = ++i % 3;
+    // manager.print_modules();
+    std::vector<module*>* nodes = new std::vector<module*>[3];
+    std::vector<int>* moduleCount = new std::vector<int>;
+    moduleCount->resize(3);
+
+    node_divider divider;
+    divider.divide_modules(manager.get_modules(), nodes, moduleCount);
+
+    for (int i = 0; i < 3; i++) {
+        std::vector<module*> nodes_modules = nodes[i];
+        std::cout << "Node " << i << std::endl;
+
+        for (module* mod : nodes_modules) {
+            mod->print_sons();
+        }
+        std::cout << std::endl;
     }
+
     manager.get_instructions(3);
-    manager.print_assigned_processes();
-    manager.print_separate_instructions();
-    std::string instr = manager.get_instructions_for_process(0);
+    // manager.print_assigned_processes();
+    // manager.print_separate_instructions();
+    std::string instr = manager.get_instructions_for_process(2);
     std::cout << instr << std::endl;
 
     return 0;
---
+++ DDD/utils/divider.cpp
@@ -0,0 +1,64 @@
+#include "divider.hpp"
+
+/*
+ * @brief Checks if modules can be divided.
+ *
+ * Throws an error if no modules are provided.
+ */
+void divider::check(std::vector<module*>* modules) {
+    if (modules->empty()) {
+        std::cerr << "There are no modules to divide.\n";
+        exit(3);
+    }
+}
+
+/*
+ * @brief Divides modules among nodes evenly based on no particular property.
+ */
+void node_divider::divide_modules(std::vector<module*>* modules, std::vector<module*> nodes[],
+                                  std::vector<int>* count) {
+    this->check(modules);
+
+    int nodeUsed = 0;
+
+    for (module* mod : *modules) {
+        nodes[nodeUsed].push_back(mod);
+        count->at(nodeUsed)++;
+        mod->assign_process(nodeUsed);
+        nodeUsed = (nodeUsed + 1) % count->size();
+    }
+}
+
+/*
+ * @brief Divides modules among nodes evenly based on how many variables modules have.
+ */
+void var_count_divider::divide_modules(std::vector<module*>* modules, std::vector<module*> nodes[],
+                                       std::vector<int>* count) {
+    this->check(modules);
+
+    std::vector<int> nodeVarCount;
+    nodeVarCount.resize(count->size());
+
+    std::sort(modules->begin(), modules->end(),
+              [](module* a, module* b) { return a->get_var_count() > b->get_var_count(); });
+
+    for (module* mod : *modules) {
+        int modVarCount = mod->get_var_count();
+        int nodeUsed = 0;
+
+        for (int i = 0; i < nodeVarCount.size(); i++) {
+            if (nodeVarCount.at(i) == 0) {
+                nodeUsed = i;
+                break;
+            }
+
+            if (nodeVarCount.at(nodeUsed) > nodeVarCount.at(i)) {
+                nodeUsed = i;
+            }
+        }
+        nodeVarCount.at(nodeUsed) += modVarCount;
+        nodes[nodeUsed].push_back(mod);
+        count->at(nodeUsed)++;
+        mod->assign_process(nodeUsed);
+    }
+}
