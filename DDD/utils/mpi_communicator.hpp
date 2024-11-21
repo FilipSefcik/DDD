1909c4c6408064b8c3231f322fa05aad45bb5338
FilipSefcik <sefcik.filip02@gmail.com>
2024-11-22 00:05:32 +0100

    new type of message system tried

--- DDD/utils/mpi_communicator.hpp
+++ DDD/utils/mpi_communicator.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <mpi.h>
 #include <string>
+#include <vector>
 
 /*
  * @brief mpi_communicator simplifies usage of MPI message functions.
@@ -20,5 +21,11 @@ class mpi_communicator {
     static void recv_message(int senderRank, mpi_message& message);
 
     static void scatter_ints(int* message, int* receiveBuffer);
+    static void scatter_messages(const std::vector<mpi_message>& messages,
+                                 mpi_message& receiveBuffer);
     static void gather_doubles(double* message, double* receiveBuffer);
+
+  private:
+    static void serialize_message(const mpi_message message, std::string& serializedMessage);
+    static void deserialize_message(const std::string& serializedMessage, mpi_message& message);
 };
