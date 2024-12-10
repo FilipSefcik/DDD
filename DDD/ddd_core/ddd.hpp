bc95e6d2c041fd57104392c6ad33b0c345382f63
FilipSefcik <sefcik.filip02@gmail.com>
2024-12-10 23:19:07 +0100

    finished core program with user interface

---
+++ DDD/ddd_core/ddd.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "../processing/process.hpp"
+
+/*
+ * @brief bdd_distributor is used as an interface to control processing of config file
+ *
+ * You can set a path to config file,
+ * start calculating availability where you define used divider, value that you want to be
+ * calculated and usage of timer, recieve the time of how long the calculating took.
+ */
+class ddd {
+  private:
+    int my_rank, process_count;
+    double start_time, end_time;
+    process* process_;
+
+  public:
+    ddd();
+    void set_conf_path(std::string pa_conf_path);
+    void calculate_availability(int divider_flag, int state, bool timer_on);
+    void get_max_time();
+    ~ddd();
+};
