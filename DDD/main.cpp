081f37a6f88286651afca7a48732ba34cc50c2f8
FilipSefcik <sefcik.filip02@gmail.com>
2024-12-10 23:19:07 +0100

    finished core program with user interface

--- DDD/main.cpp
+++ DDD/main.cpp
@@ -1,49 +1,37 @@
-// #include "modules/module.hpp"
-// #include "processing/module_manager.hpp"
-// #include "processing/mpi_manager.hpp"
-#include "processing/process.hpp"
-// #include "utils/divider.hpp"
-// #include "utils/mpi_communicator.hpp"
-// #include <cstddef>
-// #include <cstddef>
-#include <cstdlib>
-#include <iostream>
-// #include <ostream>
-#include <string>
-// #include <vector>
+#include "ddd_core/ddd.hpp"
 
+/*
+ * @brief this function is called when app is used. Processes information based on command line
+ * input arguments.
+ *
+ * To start main you should use command:
+ * mpiexec <0> -n <1> <2> <3> <4> <5> <6>
+ * \Where:
+ * \0 - usage of threads as process (optional, default are cores), if yes use --use-hwthread-cpus
+ * \1 - number of processes
+ * \2 - path to this main
+ * \3 - path to config file
+ * \4 - which divider should be used (0 for var_count_divider or 1 for node_divider)
+ * \5 - availability which state (0 or 1) should be calculated
+ * \6 - usage of timer (y or n)
+ */
 int main(int argc, char* argv[]) {
-    MPI_Init(&argc, &argv);
-
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-    int processCount;
-    MPI_Comm_size(MPI_COMM_WORLD, &processCount);
-
-    char processorName[MPI_MAX_PROCESSOR_NAME];
-    int nameLength;
-    MPI_Get_processor_name(processorName, &nameLength);
-
-    // std::cout << "Rank " << rank << " runs on " << processorName << std::endl;
-
-    process* process = nullptr;
-
-    if (rank == 0) {
-        auto* mainProcess = new main_process(rank, processCount);
-        mainProcess->set_conf_path("/home/sefcik1/mpi_cloud/modules/module_map.conf");
-        mainProcess->set_divider(1);
-        process = mainProcess;
-    } else {
-        process = new slave_process(rank);
+    std::string conf_path = argc > 1 ? argv[1] : "module_map.conf";
+    int divider_flag = argc > 2 ? std::stoi(argv[2]) : 0;
+    int state = argc > 3 ? std::stoi(argv[3]) : 1;
+    bool timer = false;
+    if (argc > 4) {
+        std::string yes = "y";
+        if (yes == argv[4]) {
+            timer = true;
+        }
     }
 
-    process->process_information();
-    process->process_instructions(0);
-
-    delete process; // Virtual destructor ensures proper cleanup
-    //  std::cout << rank << " PROCESS DELETE OK\n";
-
-    MPI_Finalize();
+    ddd distributor;
+    distributor.set_conf_path(conf_path);
+    distributor.calculate_availability(divider_flag, state, timer);
+    if (timer) {
+        distributor.get_max_time();
+    }
     return 0;
 }
