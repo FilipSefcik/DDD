1f31c2e5071576b59eaa76a4b75fb6ca388d86d2
FilipSefcik <sefcik.filip02@gmail.com>
2025-02-06 00:58:20 +0100

    added and tested callbacks for divide and add instrucion

--- DDD/utils/callbacks.cpp
+++ DDD/utils/callbacks.cpp
@@ -50,3 +50,25 @@ bool divide_by_var_count(std::vector<module_info*>* modules, int nodeCount) {
 
     return true;
 }
+
+void add_instruction_density(module_info* mod, std::string* instructions) {
+        module_info* parent = mod->get_parent();
+
+        // EXEC - module name - position of the module in parent
+        *instructions += "EXEC " + mod->get_name() + " " + std::to_string(mod->get_position()) + "\n";
+        if (parent) {
+            if (mod->get_assigned_process() == parent->get_assigned_process()) {
+                // LINK - name of parent module - name of son module
+                *instructions += "LINK " + parent->get_name() + " " + mod->get_name() + "\n";
+            } else {
+                // SEND - name of module - rank of the process to send
+                *instructions += "SEND " + mod->get_name() + " " + std::to_string(parent->get_assigned_process()) + "\n";
+
+                // RECV - parent module name - rank of the process received from
+                *(instructions + 1) += "RECV " + parent->get_name() + " " + std::to_string(mod->get_assigned_process()) + "\n";
+            }
+        } else {
+            // END - module which gives answer
+            *instructions += "END " + mod->get_name() + "\n";
+        }
+}
