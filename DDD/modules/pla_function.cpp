52c13cba8611904d2178874c30d885a3df013df0
FilipSefcik <sefcik.filip02@gmail.com>
2025-04-05 23:56:01 +0200

    function loads from pla file

--- DDD/modules/pla_function.cpp
+++ DDD/modules/pla_function.cpp
@@ -1,39 +1,84 @@
 #include "pla_function.hpp"
+#include <cstddef>
+#include <fstream>
 #include <ostream>
+#include <sstream>
 #include <string>
 #include <vector>
 
 pla_function::pla_function() {
-    this->variables = new std::vector<std::string>();
-    this->fun_values = new std::vector<int>();
+    this->variables_ = new std::vector<std::string>();
+    this->fun_values_ = new std::vector<int>();
 }
 
 pla_function::~pla_function() {
-    delete this->variables;
-    delete this->fun_values;
+    delete this->variables_;
+    delete this->fun_values_;
 }
 
-std::vector<std::string>* pla_function::get_vars_from_fun(int function_value) {
+void pla_function::load_from_pla(const std::string& filePath) {
+
+    std::ifstream inputFile(filePath);
+    if (! inputFile.is_open()) {
+        throw std::runtime_error("Error opening file: " + filePath);
+    }
+
+    std::string line;
+    int varCount = 0;
+
+    while (std::getline(inputFile, line)) {
+        // Preskoč prázdne riadky, komentáre a príkazy začínajúce bodkou
+        if (line.empty() || line[0] == '#') {
+            continue;
+        }
+
+        if (line[0] == '.') {
+            // Ak ide o riadok s počtom vstupov, napr. ".i 5"
+            if (line.size() > 1 && line[1] == 'i') {
+                std::istringstream iss(line);
+                std::string token;
+                iss >> token; // token by mal byť ".i"
+                iss >> varCount; // načíta počet vstupných premenných
+            }
+            continue;
+        }
+
+        if (varCount == 0) {
+            throw std::runtime_error("Input variable count not defined in file: " + filePath);
+        }
+
+        // Prvých var_count znakov predstavuje bitové slovo (premenné)
+        std::string variables = line.substr(0, varCount);
+        int value = std::stoi(line.substr(varCount + 1));
+
+        // Pridaj načítaný riadok do PLA funkcie
+        this->add_line(variables, value);
+    }
+
+    inputFile.close();
+}
+
+std::vector<std::string>* pla_function::get_vars_from_fun(int functionValue) {
     std::vector<std::string>* all_vars = new std::vector<std::string>;
 
-    for (int i = 0; i < this->fun_values->size(); i++) {
-        if (this->fun_values->at(i) == function_value) {
-            all_vars->push_back(this->variables->at(i));
+    for (int i = 0; i < this->fun_values_->size(); i++) {
+        if (this->fun_values_->at(i) == functionValue) {
+            all_vars->push_back(this->variables_->at(i));
         }
     }
 
     return all_vars;
 }
 
-void pla_function::add_line(std::string new_vars, int value) {
-    this->add_variables(new_vars);
+void pla_function::add_line(std::string newVars, int value) {
+    this->add_variables(newVars);
     this->add_fun_value(value);
 }
 
 void pla_function::print_function() {
     std::cout << "variables       function\n";
-    for (int i = 0; i < this->fun_values->size(); i++) {
-        std::cout << this->variables->at(i) << " " << this->fun_values->at(i) << std::endl;
+    for (int i = 0; i < this->fun_values_->size(); i++) {
+        std::cout << this->variables_->at(i) << " " << this->fun_values_->at(i) << std::endl;
     }
 }
 
@@ -41,9 +86,9 @@ std::vector<std::vector<std::string>*>* pla_function::sort_by_function() {
     std::vector<std::vector<std::string>*>* sorted = new std::vector<std::vector<std::string>*>();
     for (int i = 0; i < 2; i++) {
         std::vector<std::string>* new_function = new std::vector<std::string>();
-        for (int j = 0; j < this->fun_values->size(); j++) {
-            if (this->fun_values->at(j) == i) {
-                new_function->push_back(this->variables->at(j));
+        for (int j = 0; j < this->fun_values_->size(); j++) {
+            if (this->fun_values_->at(j) == i) {
+                new_function->push_back(this->variables_->at(j));
             }
         }
         sorted->push_back(new_function);
@@ -56,12 +101,12 @@ std::vector<std::vector<std::string>*>* pla_function::sort_by_position(int posit
     std::vector<std::string>* whatever_lines = new std::vector<std::string>();
     for (int i = 0; i < 2; i++) {
         std::vector<std::string>* new_function = new std::vector<std::string>();
-        for (int j = 0; j < this->variables->size(); j++) {
-            if (this->variables->at(j)[position] == '-' && i == 0) {
-                whatever_lines->push_back(this->variables->at(j));
+        for (int j = 0; j < this->variables_->size(); j++) {
+            if (this->variables_->at(j)[position] == '-' && i == 0) {
+                whatever_lines->push_back(this->variables_->at(j));
             }
-            if (this->variables->at(j)[position] - '0' == i) {
-                new_function->push_back(this->variables->at(j));
+            if (this->variables_->at(j)[position] - '0' == i) {
+                new_function->push_back(this->variables_->at(j));
             }
         }
         sorted->push_back(new_function);
@@ -70,18 +115,18 @@ std::vector<std::vector<std::string>*>* pla_function::sort_by_position(int posit
     return sorted;
 }
 
-void pla_function::input_variables(std::vector<std::vector<std::string>*>* additional_vars,
+void pla_function::input_variables(std::vector<std::vector<std::string>*>* additionalVars,
                                    int position, int function) {
     std::vector<std::string>* new_vars = new std::vector<std::string>();
     std::vector<int>* new_fun_vals = new std::vector<int>();
 
     std::vector<std::vector<std::string>*>* my_vars = this->sort_by_position(position);
     std::string temp_line, input_line;
-    int var_count = additional_vars->at(0)->at(0).length();
+    int var_count = additionalVars->at(0)->at(0).length();
     int fun_value;
     std::string whatever_input = std::string(var_count, '-');
 
-    for (int i = 0; i < my_vars->size(); i++) {
+    for (size_t i = 0; i < my_vars->size(); i++) {
         if (i == my_vars->size() - 1) {
             for (int l = 0; l < my_vars->at(i)->size(); l++) {
 
@@ -98,9 +143,9 @@ void pla_function::input_variables(std::vector<std::vector<std::string>*>* addit
         for (int j = 0; j < my_vars->at(i)->size(); j++) {
             temp_line = my_vars->at(i)->at(j);
             fun_value = this->get_fun_value(temp_line);
-            for (int k = 0; k < additional_vars->at(i)->size(); k++) {
+            for (int k = 0; k < additionalVars->at(i)->size(); k++) {
                 temp_line = my_vars->at(i)->at(j);
-                input_line = additional_vars->at(i)->at(k);
+                input_line = additionalVars->at(i)->at(k);
 
                 temp_line = this->replace_char(temp_line, position, input_line);
 
@@ -110,21 +155,21 @@ void pla_function::input_variables(std::vector<std::vector<std::string>*>* addit
         }
     }
 
-    this->variables = new_vars;
-    this->fun_values = new_fun_vals;
+    this->variables_ = new_vars;
+    this->fun_values_ = new_fun_vals;
 }
 
-int pla_function::get_fun_value(std::string indexed_variables) {
+int pla_function::get_fun_value(std::string indexedVariables) {
     // Find the iterator to the element
-    auto it = std::find(this->variables->begin(), this->variables->end(), indexed_variables);
+    auto it = std::find(this->variables_->begin(), this->variables_->end(), indexedVariables);
 
     int value = 0;
 
     // Check if the element was found
-    if (it != this->variables->end()) {
+    if (it != this->variables_->end()) {
         // Calculate the index
-        int index = std::distance(this->variables->begin(), it);
-        value = this->fun_values->at(index);
+        int index = std::distance(this->variables_->begin(), it);
+        value = this->fun_values_->at(index);
     }
 
     return value;
--- DDD/modules/pla_function.hpp
+++ DDD/modules/pla_function.hpp
@@ -5,24 +5,25 @@
 
 class pla_function {
   private:
-    std::vector<std::string>* variables;
-    std::vector<int>* fun_values;
+    std::vector<std::string>* variables_;
+    std::vector<int>* fun_values_;
 
   public:
     pla_function();
     ~pla_function();
-    std::vector<std::string>* get_vars_from_fun(int function_value);
+    void load_from_pla(const std::string& filePath);
+    std::vector<std::string>* get_vars_from_fun(int functionValue);
     std::vector<std::vector<std::string>*>* sort_by_function();
     std::vector<std::vector<std::string>*>* sort_by_position(int position);
     std::string replace_char(std::string before, int position, std::string input);
-    int get_fun_value(std::string indexed_variables);
+    int get_fun_value(std::string indexedVariables);
 
-    void add_line(std::string new_vars, int value);
+    void add_line(std::string newVars, int value);
 
-    void add_fun_value(int value) { this->fun_values->push_back(value); };
-    void add_variables(std::string new_vars) { this->variables->push_back(new_vars); };
+    void add_fun_value(int value) { this->fun_values_->push_back(value); };
+    void add_variables(std::string newVars) { this->variables_->push_back(newVars); };
 
-    void input_variables(std::vector<std::vector<std::string>*>* additional_vars, int position,
+    void input_variables(std::vector<std::vector<std::string>*>* additionalVars, int position,
                          int function);
 
     void print_function();
