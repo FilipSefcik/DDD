ef97ee32f7d4a48ff9b49bab63c3707e1950a20b
FilipSefcik <sefcik.filip02@gmail.com>
2025-11-21 17:16:54 +0100

    script for generating conf files

---
+++ scripts/parallel/create_conf.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Function to calculate the polynomial
+calculate_polynomial() {
+    local power=$1
+    local result=0
+    local a=1
+    for ((i = 0; i <= power; i++)); do
+        result=$((result + a))
+        a=$((a * $2))
+    done
+    echo "$result"
+}
+
+module_count=$(calculate_polynomial "$1" "$2")
+parent_count=$(calculate_polynomial $(($1 - 1)) "$2")
+
+# echo "$module_count"
+# echo "$parent_count"
+
+output_file="../../load_files/conf_files/experiment_map.conf"
+
+# Truncate the file
+> "$output_file"
+#$((RANDOM % 45)) output count insted of 45
+for ((i = 0; i < $module_count; i++)); do
+    echo "M$i ../load_files/conf_files/$3 0" >> "$output_file"
+done
+
+echo "" >> "$output_file"
+
+for ((i = 0; i < $module_count; i++)); do
+    echo -n "M$i " >> "$output_file"
+    for ((j = 1; j <= $2; j++)); do
+        if [ "$i" -lt $parent_count ]; then
+            echo -n "M$(($j + $2 * $i))" >> "$output_file"
+        else
+            echo -n "V" >> "$output_file"
+        fi
+    done
+    echo "" >> "$output_file"
+done
+
+# echo "Done."
